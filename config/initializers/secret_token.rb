5924c88d8a846d58e352bc169327e80c75ffd202
Daniel Kehoe <daniel@danielkehoe.com>
2011-09-16 14:48:35 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fengshuiexpert::Application.config.secret_token = '7353011acd5a0775d84997300f2ae635545d78f4bb85e701ce187fa02e98cbf0ceb02b9fd49ed5650624aa67ce4535c456d7fe5cf1d73b4c4337d74eb33118c2'
